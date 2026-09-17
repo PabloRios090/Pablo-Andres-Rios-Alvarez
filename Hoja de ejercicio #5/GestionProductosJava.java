4dd5304aa90ed8a5c4b8484429f74d3621bba02d
Pablo Rios <riosalvarezpabloandres@gmail.com>
2026-09-17 13:15:44 -0600

    Ejercicio 1 completado

---
+++ Hoja de ejercicio #5/GestionProductosJava.java
@@ -0,0 +1,80 @@
+
+
+package com.mycompany.gestionproductos.java;
+import java.util.ArrayList;
+
+class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(int id, String nombre, double precio, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{id=" + id + ", nombre='" + nombre + "', precio=" + precio + ", stock=" + stock + "}";
+    }
+}
+
+public class GestionProductosJava {
+
+    public static void actualizarPrecioStockBajo(ArrayList<Producto> listaProductos, double porcentajeAumento) {
+        for (Producto producto : listaProductos) {
+            if (producto.getStock() < 10) {
+                double precioActual = producto.getPrecio();
+                double nuevoPrecio = precioActual * (1 + (porcentajeAumento / 100));
+                producto.setPrecio(nuevoPrecio);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Producto> listaProductos = new ArrayList<>();
+
+        listaProductos.add(new Producto(1, "Teclado", 150.00, 5));
+        listaProductos.add(new Producto(2, "Mouse", 80.00, 15));
+        listaProductos.add(new Producto(3, "Monitor", 1200.00, 8));
+
+        System.out.println("--- Precios iniciales ---");
+        for (Producto p : listaProductos) {
+            System.out.println(p);
+        }
+
+        actualizarPrecioStockBajo(listaProductos, 10);
+
+        System.out.println("\n--- Precios actualizados ---");
+        for (Producto p : listaProductos) {
+            System.out.println(p);
+        }
+    }
+}
